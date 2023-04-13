406a7fd93c78fdf38cb9b240824f18c5f677174b
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 11:09:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import ks_2samp, chisquare
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Критерий Колмогорова-Смирнова для двух выборок
+    alpha = 0.08
+    stat, p = ks_2samp(x, y)
+    return p < alpha
