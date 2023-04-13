4cd5d45e9094edff98a8b23b86f7a1aebd4c9b11
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 18:26:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,11 +6,5 @@ from scipy.stats import chisquare
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Статистика Хи-квадрат для двух выборок
-    alpha = 0.08
-    bins = 10 # количество интервалов
-    _, bins = np.histogram(np.concatenate((x,y)), bins=bins)
-    x_counts, _ = np.histogram(x, bins=bins)
-    y_counts, _ = np.histogram(y, bins=bins)
-    stat, p = chisquare(y_counts, f_exp=x_counts)
-    return p < alpha
+    res = stats.cramervonmises_2samp(x, y)
+    return res.pvalue < 0.03
