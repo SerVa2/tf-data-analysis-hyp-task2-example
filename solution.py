c8363e1a62c4f8778cd8a8f5b3f4e69caa8b8f93
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 17:49:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import ks_2samp, chisquare
 
+from scipy.stats import chisquare
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Критерий Колмогорова-Смирнова для двух выборок
+    # Статистика Хи-квадрат для двух выборок
     alpha = 0.08
-    stat, p = ks_2samp(x, y)
+    bins = 10 # количество интервалов
+    _, bins = np.histogram(np.concatenate((x,y)), bins=bins)
+    x_counts, _ = np.histogram(x, bins=bins)
+    y_counts, _ = np.histogram(y, bins=bins)
+    stat, p = chisquare(y_counts, f_exp=x_counts)
     return p < alpha
