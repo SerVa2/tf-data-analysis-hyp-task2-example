a87113f4b85534962cc0a8bf385f23f3317bfbd7
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 19:00:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-from scipy.stats import chisquare
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     res = stats.cramervonmises_2samp(x, y)
-    return res.pvalue < 0.03
+    return res.pvalue < 0.08
